6b38f86d8a11c4d6728f8dc41e4d5a9d14f53c4f
Rodrigogj8 <arcandroid92@gmail.com>
2025-04-21 17:02:29 -0300

    Initial commit

---
+++ apod_viewer.py
@@ -0,0 +1,23 @@
+import requests
+import webbrowser
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+api_key = os.getenv('API_KEY')
+
+url = f'https://api.nasa.gov/planetary/apod?api_key={api_key}'
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+    if data['media_type'] == 'image':
+        print(data['title'])
+        print(data['explanation'])
+
+        webbrowser.open(data['url'])
+else:
+    print("Erro ao acessar a API da NASA.")
